02b168e4a18bbeaea38a68fd036a556e0144c841
viniii05 <vinivt.0520@gmail.com>
2024-08-05 16:30:05 +0530

    using app.listen

--- task.js
+++ task.js
@@ -11,9 +11,8 @@ app.use((req,res,next) => {
 
 app.use((req,res,next) => {
     console.log('In the second middleware!');
+    res.send('<h1>Hello from express!</h1>');
 })
 
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
